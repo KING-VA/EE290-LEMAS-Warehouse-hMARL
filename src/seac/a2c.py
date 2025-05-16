b6273cf63017c4fd57674fde02ff420d789371c9
Venkata Alapati <VAlapati@lineagelogistics.com>
2025-05-16 10:34:43 -0700

    Saving work, trying more things to see if agents are able to learn anything

--- src/seac/a2c.py
+++ src/seac/a2c.py
@@ -33,8 +33,8 @@ def config():
 
     seac_coef = 1.0
 
-    num_processes = 32
-    num_steps = 100
+    num_processes = 2
+    num_steps = 10
 
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
